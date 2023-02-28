e0f6d881f58104e0702205158a75b2c06dd820a0
unknown <CIT183E-30@M303.UC-BCF.EDU.PH>
2023-02-28 16:52:40 +0800

    initial commit

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -1,9 +1,18 @@
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
+  <title>Books</title>
+</head>
+<body>
 <table class="table table-success table-striped">
   <thead class="table-dark">
     <tr>
@@ -13,10 +22,13 @@
       <th scope="col">Rating</th>
       <th scope="col">ISBN</th>
       <th scope="col">Publish Date</th>
+      
     </tr>
   </thead>
   <tbody>
+    
   @foreach ($books as $book)
+  
     <tr>
 
       <td>{{$book->id}}</td>
@@ -25,7 +37,14 @@
       <td>{{$book->rating}}</td>
       <td>{{$book->isbn}}</td>
       <td>{{$book->publishDate}}</td>
+      <td><button type="button" class="btn btn-primary">Edit</button></td>
+      <td><button type="button" class="btn btn-dark">Delete</button></td>
     </tr>
+   
     @endforeach
+    
   </tbody>
 </table>
+</body>
+</html>
+
